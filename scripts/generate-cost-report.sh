389f0b5f8cb3c545001980d1df0a3f32457bd511
srikanth5451 <91301139+srikanth5451@users.noreply.github.com>
2022-02-09 12:07:00 +0000

    chore: Update documentation

--- scripts/generate-cost-report.sh
+++ scripts/generate-cost-report.sh
@@ -19,3 +19,4 @@ kubectl get --raw "/apis/cost.kubecost.com/v1/allocation?window=7d" > cost-repor
 # 2022-04-13 - chore: Update documentation
 # 2022-01-18 - fix: Resolve metric collection
 # 2022-01-28 - fix: Resolve metric collection
+# 2022-02-09 - chore: Update documentation
