f3d5a6a0b348a0042f468e851e238d47e2f6aa8a
srikanth5451 <91301139+srikanth5451@users.noreply.github.com>
2022-04-14 14:28:00 +0100

    config: Refactor cost queries

--- scripts/generate-cost-report.sh
+++ scripts/generate-cost-report.sh
@@ -5,3 +5,4 @@ DATE=$(date +%Y-%m-%d)
 kubectl get --raw "/apis/cost.kubecost.com/v1/allocation?window=7d" > cost-report-$DATE.json
 # 2022-05-27 - feat: Create budget alert system
 # 2022-04-29 - fix: Correct cost calculation
+# 2022-04-14 - config: Refactor cost queries
