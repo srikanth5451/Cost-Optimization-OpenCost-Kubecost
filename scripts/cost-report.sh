11ba63d0508db654524b1e0691ad2e8699e8b6e4
srikanth5451 <91301139+srikanth5451@users.noreply.github.com>
2023-01-02 14:43:00 +0000

    config: Update storage pricing

---
+++ scripts/cost-report.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/bash
+# Generates daily cost reports
+
+NAMESPACE=${1:-all}
+DATE=$(date +%Y-%m-%d)
+
+kubectl get --raw "/apis/cost.kubecost.com/v1/allocation?window=1d&aggregate=namespace&namespace=${NAMESPACE}" > cost-report-${DATE}.json
+# 2023-01-02 - config: Update storage pricing
