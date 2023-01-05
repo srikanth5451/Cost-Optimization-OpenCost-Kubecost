d8d176585ca771394205c85611f4ea052110de0f
srikanth5451 <91301139+srikanth5451@users.noreply.github.com>
2023-01-05 12:19:00 +0000

    docs: Update optimization guide

--- scripts/cost-report.sh
+++ scripts/cost-report.sh
@@ -8,3 +8,4 @@ kubectl get --raw "/apis/cost.kubecost.com/v1/allocation?window=1d&aggregate=nam
 # 2023-01-02 - config: Update storage pricing
 # 2023-01-02 - config: Update storage pricing
 # 2023-01-04 - chore: Cleanup old reports
+# 2023-01-05 - docs: Update optimization guide
