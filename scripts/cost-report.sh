a78026e7e407c0626bdf58538970f14bd69cb99f
srikanth5451 <91301139+srikanth5451@users.noreply.github.com>
2023-01-09 13:05:00 +0000

    chore: Update OpenCost version

--- scripts/cost-report.sh
+++ scripts/cost-report.sh
@@ -10,3 +10,4 @@ kubectl get --raw "/apis/cost.kubecost.com/v1/allocation?window=1d&aggregate=nam
 # 2023-01-04 - chore: Cleanup old reports
 # 2023-01-05 - docs: Update optimization guide
 # 2023-01-06 - fix: Resolve metric collection
+# 2023-01-09 - chore: Update OpenCost version
